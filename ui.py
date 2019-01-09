d87e5c705500908e739b0993d02528ab48457219
Michael Fürst <mail@michaelfuerst.de>
2019-01-09 14:33:58 +0100

    Removed debug from electron and added flag to request path to the index.html for integration in other apps.

--- ui.py
+++ ui.py
@@ -1,14 +1,22 @@
 import webbrowser
 import os
+import sys
 import subprocess
 from subprocess import CalledProcessError
 
 if __name__ == "__main__":
     folder = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
-    try:
-        subprocess.check_call('npm install && npm start', shell=True, cwd=folder + "/electron")
-    except CalledProcessError as e:
-        if e.returncode == 127:
-            webbrowser.open('file:///'+ folder + '/sites/index.html')
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--path":
+            print(folder + '/sites/index.html')
         else:
-            print(e)
+            print("Wrong usage: python -m gpm.ide.ui [--path]")
+    else:
+        try:
+            subprocess.check_call('npm install && npm start', shell=True, cwd=folder + "/electron")
+        except CalledProcessError as e:
+            if e.returncode == 127:
+                webbrowser.open('file:///'+ folder + '/sites/index.html')
+            else:
+                print(e)
